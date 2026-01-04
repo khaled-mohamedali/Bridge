a0a8bb5d706e489459798be2fa306f54ec4576cc
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2026-01-04 17:19:34 -0500

    Initial Commit

---
+++ Bridge/BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeApp.swift
+//  Bridge
+//
+//  Created by Khaled Mohamed Ali on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bridge
+//
+//  Created by Khaled Mohamed Ali on 1/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
