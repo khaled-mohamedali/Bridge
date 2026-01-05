37a03e26ec6942647aa201f8ff0ecb932db9b432
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2026-01-05 14:08:58 -0500

    Created Bleutooth Scanning VIew

---
+++ Bridge/Views/BleutoothConnectView.swift
@@ -0,0 +1,20 @@
+//
+//  BleutoothConnectView.swift
+//  Bridge
+//
+//  Created by Khaled Mohamed Ali on 1/5/26.
+//
+
+import SwiftUI
+
+struct BleutoothConnectView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    BleutoothConnectView()
+}
